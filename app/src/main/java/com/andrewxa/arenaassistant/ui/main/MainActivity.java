158edc509f43128de2ce63148a382736793c5719
Сергей Фоминов <s.fominov@volgagas.com>
2018-08-02 13:36:07 +0400

    resolved problem without models

--- app/src/main/java/com/andrewxa/arenaassistant/ui/main/MainActivity.java
+++ app/src/main/java/com/andrewxa/arenaassistant/ui/main/MainActivity.java
@@ -9,14 +9,20 @@ import android.widget.TextView;
 
 import com.andrewxa.arenaassistant.ArenaAssistant;
 import com.andrewxa.arenaassistant.R;
+import com.andrewxa.arenaassistant.datasource.model.WgAccountInfo;
 import com.andrewxa.arenaassistant.datasource.network.ArenaApi;
 import com.andrewxa.arenaassistant.datasource.network.WgApi;
 import com.andrewxa.arenaassistant.ui.main.presenter.Contract;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements Contract.View {
 
@@ -51,10 +57,15 @@ public class MainActivity extends AppCompatActivity implements Contract.View {
     private void fetchData() {
         compositeDisposable.add(ArenaAssistant.getWgApi().getWgPlayerId(playerName.getText().toString())
                 .subscribeOn(Schedulers.io())
+                .flatMap((Function<WgAccountInfo, Observable<ResponseBody>>) accountInfo ->
+                        ArenaAssistant.getArenaApi().getArenaPlayerId(Long.toString(accountInfo.getData().get(0).getAccountID())))
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(accountInfo -> {
-                    playerWgName = accountInfo.getData().get(0).getNickname();
-                    playerWgId = Long.toString(accountInfo.getData().get(0).getAccountID());
+                .subscribe(body -> {
+                    Timber.d("body: " + body.string());
+                    Timber.d("body: " + body.string());
+                }, throwable -> {
+                    Timber.d("throwable: " + throwable.getCause());
+                    Timber.d("throwable: " + throwable.getMessage());
                 }));
     }
 
