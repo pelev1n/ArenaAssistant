853ca3e529e53e76c566fd65dcdf97176cfb3a05
pelev1n <pelevin9911@gmail.com>
2018-08-25 20:10:49 +0300

    Added Commaners images to Adapter

--- app/src/main/java/com/andrewxa/arenaassistant/ui/main/fragments/Commanders/CommandersAdapter.java
+++ app/src/main/java/com/andrewxa/arenaassistant/ui/main/fragments/Commanders/CommandersAdapter.java
@@ -1,21 +1,16 @@
 package com.andrewxa.arenaassistant.ui.main.fragments.Commanders;
 
-import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.andrewxa.arenaassistant.R;
 import com.andrewxa.arenaassistant.datasource.model.arenamodel.Stat;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -40,20 +35,31 @@ public class CommandersAdapter extends RecyclerView.Adapter<CommandersAdapter.Vi
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        String commanderName = commanders.get(position).getCommanderKey();
-        String commanderBattles = Long.toString(commanders.get(position).getVictories()+
-                                                + commanders.get(position).getDefeats());
-        String commanderMaxPointsCent = Long.toString(commanders.get(position).getMaxPointsCents()/100);
-        String commanderFreeXpCents = Long.toString(commanders.get(position).getFreeXPCents()/100);
-        String commanderUnitXpCents = Long.toString(commanders.get(position).getUnitXPCents()/100);
-        String commanderSilverCents = Long.toString(commanders.get(position).getSilverCents()/100);
+        /*String commanderName = commanders.get(position).getCommanderKey();
+        String commanderBattles = Long.toString(commanders.get(position).getVictories() +
+                +commanders.get(position).getDefeats());
+        String commanderMaxPointsCent = Long.toString(commanders.get(position).getMaxPointsCents() / 100);
+        String commanderFreeXpCents = Long.toString(commanders.get(position).getFreeXPCents() / 100);
+        String commanderUnitXpCents = Long.toString(commanders.get(position).getUnitXPCents() / 100);
+        String commanderSilverCents = Long.toString(commanders.get(position).getSilverCents() / 100);
 
         holder.commanderName.setText("Имя: " + commanderName);
         holder.commanderBattles.setText("Битвы: " + commanderBattles);
         holder.commanderMaxPointsCent.setText("Рекорд по очкам: " + commanderMaxPointsCent);
         holder.commanderFreeXpCents.setText("Свободный опыт: " + commanderFreeXpCents);
         holder.commanderUnitXpCents.setText("Опыт отряда: " + commanderUnitXpCents);
-        holder.commanderSilverCents.setText("Серебро: " + commanderSilverCents);
+        holder.commanderSilverCents.setText("Серебро: " + commanderSilverCents);*/
+
+
+        String commanderName = commanders.get(position).getCommanderKey();
+        Glide.with(context).load(getImage(commanderName)).into(holder.commanderView);
+
+        holder.commanderName.setText(getCorrectName(commanderName));
+/*        Contact contact = mContacts.get(position);
+        holder.rootView.setTag(contact);
+        holder.tvName.setText(contact.getName());
+
+        Glide.with(context).load(contact.getThumbnailDrawable()).centerCrop().into(holder.ivProfile);*/
     }
 
     @Override
@@ -61,24 +67,50 @@ public class CommandersAdapter extends RecyclerView.Adapter<CommandersAdapter.Vi
         return commanders.size();
     }
 
+    public int getImage(String imageName) {
+        int drawableResourceId = context.getResources().getIdentifier(imageName, "drawable", context.getPackageName());
+        return drawableResourceId;
+    }
+
+    public String getCorrectName(String jsonName) {
+
+        return "s";
+    }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView commanderName;
+        /*TextView commanderName;
         TextView commanderBattles;
         TextView commanderMaxPointsCent;
         TextView commanderFreeXpCents;
         TextView commanderUnitXpCents;
-        TextView commanderSilverCents;
+        TextView commanderSilverCents;*/
 
-        ViewHolder(View view){
+        final ImageView commanderView;
+        final TextView commanderName;
+        final View vPalette;
+
+        ViewHolder(View view) {
             super(view);
-            commanderName = (TextView) view.findViewById(R.id.commander_name);
+
+            commanderView = (ImageView) itemView.findViewById(R.id.commander_view);
+            commanderName = (TextView) itemView.findViewById(R.id.commander_name);
+            vPalette = itemView.findViewById(R.id.vPalette);
+
+
+
+
+            /*commanderName = (TextView) view.findViewById(R.id.commander_name);
             commanderBattles = (TextView) view.findViewById(R.id.commander_battles);
             commanderMaxPointsCent = (TextView) view.findViewById(R.id.commander_max_points_cent);
             commanderFreeXpCents = (TextView) view.findViewById(R.id.commander_free_xp_cents);
             commanderUnitXpCents = (TextView) view.findViewById(R.id.commander_unit_xp_cents);
-            commanderSilverCents = (TextView) view.findViewById(R.id.commander_silver_cents);
+            commanderSilverCents = (TextView) view.findViewById(R.id.commander_silver_cents);*/
         }
+
+
+
+
     }
 }
 
