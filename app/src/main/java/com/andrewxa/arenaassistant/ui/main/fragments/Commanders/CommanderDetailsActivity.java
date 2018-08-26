7b3449ec6f4f4eec2642e9cf8ac8c261517730fb
pelev1n <pelevin9911@gmail.com>
2018-08-26 22:45:55 +0300

    Added CommanderDetailsActivity

---
+++ app/src/main/java/com/andrewxa/arenaassistant/ui/main/fragments/Commanders/CommanderDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.andrewxa.arenaassistant.ui.main.fragments.Commanders;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CommanderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
